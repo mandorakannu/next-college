cd47a4891485a3b6d2d8ea758c2e15b064ef9ed0
mandorakannu <mandorakannu@gmail.com>
2024-06-06 16:59:58 +0530

    chore: Add ReduxProvider to RootLayout for global state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ReduxProvider } from "@providers/redux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <ReduxProvider>
+      <ClerkProvider>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </ClerkProvider>
+    </ReduxProvider>
   );
 }
