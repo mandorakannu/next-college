575297c0201089657d4cf39fb02eb2d76c0758f4
mandorakannu <mandorakannu@gmail.com>
2024-06-07 17:24:30 +0530

    feat: Add navigation component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ReduxProvider } from "@providers/redux/provider";
+import { Navigation } from "@ui/shared/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,63 @@ export const metadata: Metadata = {
   title: "Core Campus",
   description:
     "Core Campus is a college management project for students and teachers. This project is made by Kannu Mandora | Full Stack Developer",
-};
+    keywords: [
+      "Kannu",
+      "Kannu Mandora",
+      "Mandora",
+      "Web Developer",
+      "Full Stack",
+      "MERN Stack",
+      "Stack",
+      "Full Stack Developer",
+      "Mandora Kannu",
+      "tech blogger",
+      "developer",
+      "web development",
+      "software engineering",
+      "programming languages",
+    ],
+    authors: [{ name: "Kannu Mandora" }],
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: false,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Kannu Mandora | Full Stack Developer",
+      description:
+        "Hi, it's me Kannu Mandora. I want to be a Full Stack Developer. I am currently focused on my skills in React, Node, and Express. I am also learning about MySQL and MongoDB.",
+      siteId: "@mandorakannu",
+      creator: "@KannuMandora",
+      creatorId: "1467726470533754880",
+      images: ["https://collegemanagement.mandorakannu.xyz/images/main.webp"],
+    },
+    verification: {
+      google: "_RMbn1Udckop0qHdoAu_PoiGFWqwB19CecNEqZfHxAE",
+      yandex: "yandex",
+      yahoo: "yahoo",
+      other: {
+        me: ["mandorakannu@gmail.com"],
+      },
+    },
+    icons: [
+      {
+        href: "/favicon.ico",
+        rel: "icon",
+        type: "image/x-icon",
+        url: "/favicon.ico",
+      },
+    ],
+  };
 
 export default function RootLayout({
   children,
@@ -21,7 +78,9 @@ export default function RootLayout({
     <ReduxProvider>
       <ClerkProvider>
         <html lang="en">
-          <body className={inter.className}>{children}</body>
+          <body className={inter.className}>
+            <Navigation />
+            {children}</body>
         </html>
       </ClerkProvider>
     </ReduxProvider>
---
+++ components/ui/shared/Navigation.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs/server";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+
+export const Navigation = async () => {
+  const user = await currentUser();
+  return (
+    <>
+      <nav className="bg-sky-600 p-4 fixed w-full">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href="/" className="text-white text-2xl font-bold">
+            Core Campus
+          </Link>
+          <ul className="flex gap-5 last:mr-4">
+            <li>
+              <Link
+                href="/"
+                className="text-white p-2 hover:text-black transition-colors delay-75"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="text-white p-2 hover:text-black transition-colors delay-75"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="text-white p-2 hover:text-black transition-colors delay-75"
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              {user === null || user === undefined ? (
+                <Link
+                  href="/sign-in"
+                  className="text-white bg-gray-400 hover:bg-orange-300 rounded-md p-2 transition-colors delay-75"
+                >
+                  Sign In
+                </Link>
+              ) : (
+                <>
+                <Link
+                  href="/dashboard"
+                  className="text-white bg-gray-400 hover:bg-orange-300 rounded-md p-2 transition-colors delay-75 mr-6"
+                >
+                  Dashboard
+                </Link>
+                <SignedIn>
+                  <SignOutButton signOutOptions={{ redirectUrl: "/sign-in" }}>
+                    <span className="text-white hover:text-black transition-colors delay-75 cursor-pointer underline underline-offset-2">
+                      Logout
+                    </span>
+                  </SignOutButton>
+                </SignedIn>
+                </>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
